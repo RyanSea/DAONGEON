4d4572e8cdf7bd4d854ff872afe18f6bb9662f37
Ryan <81717668+RyanSea@users.noreply.github.com>
2022-09-22 22:14:52 -0700

    ♲Edited Movement♲

--- src/Dungeon.sol
+++ src/Dungeon.sol
@@ -16,7 +16,7 @@ contract Dungeon {
     struct Environment {
         string location; // styled like a layered chessboard 
         NPC[] npcs;
-        ICharacter[] characters;
+        uint[] characters; // ids[]
         Item[] items;
     }
 
@@ -80,13 +80,30 @@ contract Dungeon {
 
         string memory location = player.location_exact;
 
+        uint length = layout[location].characters.length;
+
+        // remove player id from previous Environment
+        for (uint i; i < length; ++i) {
+            if(layout[location].characters[i] == _player) {
+                layout[location].characters[i] = layout[location].characters[length - 1];
+
+                layout[location].characters.pop();
+
+                break;
+            }
+        }  
+
         // utilize oz's Strings.sol to parse location..
         // psuedocode to follow:
         location = "new location";
 
+        // change player's location_exact
         player.location_exact = location;
-
         character.adjustProfile(_player, player);
+
+        // add player id to new Environment
+        layout[location].characters.push(_player);
     }
+
     
 }
